5bad27e1d757cc22226879f5e92586f557032008
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2019-09-11 17:07:31 +0800

    Added the baseOne.

--- src/testProute.py
+++ src/testProute.py
@@ -10,7 +10,13 @@
 #-----------------------------------------------------------------------------
 # https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-ii-templates
 
-from flask import Flask, render_template, flash, redirect
+import json
+import random
+import time
+from datetime import datetime
+
+
+from flask import Flask, render_template, flash, redirect, Response
 from testPGlobal import Config
 
 from flask_wtf import FlaskForm # pip install flask-wtf
@@ -21,16 +27,6 @@ application = Flask(__name__)
 application.config.from_object(Config)
 
 @application.route('/')
-@application.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect('/index')
-    return render_template('login.html', title='Sign In', form=form)
-
-
 @application.route('/index')
 def index():
     user = {'username' : 'test'}
@@ -53,6 +49,31 @@ def index():
     return render_template('index.html', title='Sign in', user=user, posts=posts)
 
 
+@application.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        print(str(form.username.data))
+        print(str(form.password.data))
+        return redirect('/chart')
+    return render_template('login.html', title='Sign In', form=form)
+
+@application.route('/chart')
+def chart():
+    return render_template('chart.html')
+
+@application.route('/chart-data')
+def chart_data():
+    def generate_random_data():
+        while True:
+            json_data = json.dumps(
+                {'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'value': random.random() * 100})
+            yield f"data:{json_data}\n\n"
+            time.sleep(1)
+
+    return Response(generate_random_data(), mimetype='text/event-stream')
 
 class LoginForm(FlaskForm):
     """ From to handle the login.
