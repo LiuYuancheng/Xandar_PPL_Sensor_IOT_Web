aacdde1a74339008b4a6a147c4b4b08b61d1a9f1
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2019-09-11 16:31:02 +0800

    Added the user log in

--- src/testProute.py
+++ src/testProute.py
@@ -10,14 +10,27 @@
 #-----------------------------------------------------------------------------
 # https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-ii-templates
 
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
 from testPGlobal import Config
 
+from flask_wtf import FlaskForm # pip install flask-wtf
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 
 application = Flask(__name__)
 application.config.from_object(Config)
 
 @application.route('/')
+@application.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
+
+
 @application.route('/index')
 def index():
     user = {'username' : 'test'}
@@ -37,7 +50,18 @@ def index():
 
     ]
 
-    return render_template('index.html', title='test', user=user, posts=posts)
+    return render_template('index.html', title='Sign in', user=user, posts=posts)
+
+
+
+class LoginForm(FlaskForm):
+    """ From to handle the login.
+    """
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
 
 if __name__ == '__main__':
     application.run(debug=True, threaded=True)
