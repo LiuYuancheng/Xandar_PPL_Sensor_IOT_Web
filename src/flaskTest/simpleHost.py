9c03f10db353a1928d043a3b931c051acd8157cd
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2019-12-17 11:52:54 +0800

    added the login.

--- src/flaskTest/simpleHost.py
+++ src/flaskTest/simpleHost.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -10,17 +10,21 @@ def hello_admin():
 def hello_guest(name):
     return 'Hello %s as a guest' %name
 
-@app.route('/user/<name>')
+@app.route('/success/<name>')
 def hello_world(name):
     if str(name).lower() == 'admin':
         return redirect(url_for('hello_admin'))
     else:
         return redirect(url_for('hello_guest', name=name))
-    return 'Hello World %s' %str(name)
 
-@app.route('/user/<name>')
-
-
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('hello_world', name=user))
+    else:
+        user = request.args.get('nm')
+        return redirect(url_for('hello_world', name=user))
 
 if __name__ == '__main__':
    app.run(host= "0.0.0.0", debug=True, threaded=True)
