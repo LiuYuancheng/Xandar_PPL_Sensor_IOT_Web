ddc4f2a9f4b4823dd44cdf33afce1bc7f3353fc8
Liu Yuancheng <liu_yuan_cheng@hotmail.com>
2022-01-31 12:12:15 +0800

    Added the user/password config.

--- src/XandaGlobal.py
+++ src/XandaGlobal.py
@@ -5,13 +5,14 @@
 #              which will be used in the other modules.
 # Author:      Yuancheng Liu
 #
-# Created:     2019/07/05 [rebuilt on 29/01/2022]
+# Created:     2019/07/05 [rebuilt on 30/01/2022]
 # version:     v_2.1
 # Copyright:   NUS – Singtel Cyber Security Research & Development Laboratory
 # License:     YC @ NUS
 #-----------------------------------------------------------------------------
 
 import os
+import platform
 
 print("Current working directory is : %s" % str(os.getcwd()))
 dirpath = os.path.dirname(__file__)
@@ -20,5 +21,14 @@ print("Current source code location : %s" % dirpath)
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Temporary_secret_key' 
 
+DE_COMM = 'COM3' if platform.system() == 'Windows' else '/dev/ttyUSB0'
+
+
+USER_PWD = os.path.join(dirpath, 'ConfigUser.txt')
+
 #-------<GLOBAL PARAMTERS>-----------------------------------------------------
+iUserMgr = None
 iCommReader = None #  Comm port read program.
+
+
+gSimulationMode = True
--- src/XandaWebHost.py
+++ src/XandaWebHost.py
@@ -26,12 +26,12 @@ from flask_wtf import FlaskForm # pip install flask-wtf
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
+import ConfigLoader as loader
 import XandaGlobal as gv
+import XAKAsensorComm as xcomm
 from XandaGlobal import Config
 
-DE_COMM = 'COM3' if platform.system() == 'Windows' else '/dev/ttyUSB0'
 
-#application = Flask(__name__)
 application = Flask(__name__, static_url_path='/static')
 application.config.from_object(Config)
 
@@ -39,14 +39,14 @@ application.config.from_object(Config)
 @application.route('/')
 @application.route('/index')
 def index():
-    user = {'username' : 'Welcome to access the XAKA people counting sensor'}
+    user = {
+        'username': ' Welcome to access the XAKA[Xandar Kardian] people counting sensor '}
     posts = [
         {
-            'author': {'username': 'new user'},
+            'author': {'username': 'New IOT user'},
             'body': 'The user has not logged in yet.'
         },
     ]
-
     return render_template('index.html', title='Sign in', user=user, posts=posts)
 
 #-----------------------------------------------------------------------------
@@ -54,10 +54,12 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if str(form.username.data) == '123' and str(form.password.data) == '123':
-            flash('Login requested for user {}, remember_me={}'.format(
-                form.username.data, form.remember_me.data))
-            return redirect('/chart')
+        userDict = gv.iUserMgr.getJson()
+        if str(form.username.data) in userDict.keys():
+            if str(userDict[form.username.data]) == str(form.password.data) :
+                flash('Login requested for user {}, remember_me={}'.format(
+                    form.username.data, form.remember_me.data))
+                return redirect('/chart')
         else:
             flash('User or password incorrect, please login again')
             return redirect('/index')
@@ -71,21 +73,21 @@ def chart():
 
 @application.route('/chart-data') # the route component must match the related <dev> in the html file.
 def chart_data():
-    def generate_random_data():
+    def generate_sensor_data():
         while True:
+            dataList = gv.iCommReader.fetchSensorData()
+            peopleNum = int(dataList[27])
             json_data = json.dumps(
                 #{'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'value': gv.iCommReader.readComm() })
-                {'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'value': random.randint(2,5) })
+                {'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'value': peopleNum })
             #print(f"data:{json_data}\n\n")
             yield "data:"+json_data+"\n\n"
             time.sleep(1)
-
-    return Response(generate_random_data(), mimetype='text/event-stream')
+    return Response(generate_sensor_data(), mimetype='text/event-stream')
 
 #-----------------------------------------------------------------------------
 class LoginForm(FlaskForm):
-    """ From to handle the login.
-    """
+    """ Form to handle the user login."""
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
@@ -93,78 +95,10 @@ class LoginForm(FlaskForm):
 
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
-class SensorCom(object):
-    """ Interface to store the PLC information and control the PLC through 
-        by hooking to the ModBus(TCPIP).
-    """
-    def __init__(self, parent):
-        self.serialPort = DE_COMM   # the serial port name we are going to read.
-        self.serComm = None # serial comm handler used to read the sensor data. 
-        self.dataList = []
-        self.setSerialComm(searchFlag=True)
-
-     #--SensorReaderFrame-----------------------------------------------------------
-    def setSerialComm(self, searchFlag=False):
-        """ Automatically search for the sensor and do the connection."""
-        if not self.serComm is None:
-            self.serComm.close()  # close the exists opened port.
-            self.serComm = None 
-        portList = []
-        if searchFlag:
-            # look for the port on different platform:
-            if sys.platform.startswith('win'):
-                ports = ['COM%s' % (i + 1) for i in range(256)]
-            elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-                # this excludes your current terminal "/dev/tty"
-                ports = glob.glob('/dev/tty[A-Za-z]*')
-            elif sys.platform.startswith('darwin'):
-                ports = glob.glob('/dev/tty.*')
-            else:
-                raise EnvironmentError('Serial Port comm connection error: Unsupported platform.')
-            for port in ports:
-                # Check whether the port can be open.
-                try:
-                    s = serial.Serial(port)
-                    s.close()
-                    portList.append(port)
-                except (OSError, serial.SerialException):
-                    pass
-            print(('COM connection: the serial port can be used :%s' % str(portList)))
-        # normally the first comm prot is resoved by the system.
-        if not self.serialPort in portList: self.serialPort = portList[-1]
-        try:
-            self.serComm = serial.Serial(self.serialPort, 115200, 8, 'N', 1, timeout=1)
-        except:
-            print("Serial connection: serial port open error.")
-            return None
-
-#-----------------------------------------------------------------------------
-    def readComm(self):
-        if self.serComm is None: 
-            print ("Serial readeing: The sensor is not connected.")
-            return None
-        output = self.serComm.read(500) # read 500 bytes and parse the data.
-        bset = output.split(b'XAKA')    # begine byte of the bytes set.
-        for item in bset:
-            # 4Bytes*37 = 148 paramters make sure the not data missing.
-            if len(item) == 148:
-                self.dataList = []
-                for idx, data in enumerate(iter(partial(io.BytesIO(item).read, 4), b'')):
-                    val = unpack('i', data) if idx == 0 or idx == 1 else unpack(
-                        '<f', data)  # get the ID and parameter number
-                    self.dataList.append(val[0])
-                break # only process the data once.
-        if len(self.dataList) == 0: 
-            print("Please check the sensor connection.")
-            return 0
-        print(self.dataList[4])
-        return self.dataList[4]
-
-
-#-----------------------------------------------------------------------------
-#-----------------------------------------------------------------------------
 if __name__ == '__main__':
-    #gv.iCommReader = SensorCom(None)
+    gv.iUserMgr = loader.ConfigLoader(gv.USER_PWD, mode='r', filterChars=('#', '', '\n'))
+    gv.iCommReader = xcomm.XAKAsensorComm(gv.DE_COMM, simuMd=gv.gSimulationMode)
+    gv.iCommReader.setSerialComm(searchFlag=True)
     print('Start the web server.')
     application.run(debug=False, threaded=True)
     # application.run(host= "0.0.0.0", debug=False, threaded=True) # use 0.0.0.0 if we want access the web from other computer.
